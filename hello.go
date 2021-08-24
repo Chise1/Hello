7bd59563a91facc76643c05b703b5cc2f1d6d279
yuxiaofei <531189371@qq.com>
2021-08-24 11:01:29 +0800

    hello: changes for v2.0.0

--- hello.go
+++ hello.go
@@ -5,7 +5,9 @@ import quoteV3 "rsc.io/quote/v3"
 func Hello() string {
 	return quoteV3.HelloV3()
 }
-
+func HelloV2() string {
+	return "Hello,I'm back."
+}
 func Proverb() string {
 	return quoteV3.Concurrency()
 }
--- hello_test.go
+++ hello_test.go
@@ -5,12 +5,18 @@ import "testing"
 func TestHello(t *testing.T) {
 	want := "Hello, world."
 	if got := Hello(); got != want {
-		t.Errorf("Hello() =%q,want %q.", got, want)
+		t.Errorf("Hello() = %q,want %q.", got, want)
 	}
 }
 func TestProberb(t *testing.T) {
 	want := "Concurrency is not parallelism."
 	if got := Proverb(); got != want {
-		t.Errorf("Proverb() = %q,want %q", got, want)
+		t.Errorf("Proverb() = %q,want %q.", got, want)
+	}
+}
+func TestHelloV2(t *testing.T) {
+	want := "Hello,I'm back."
+	if got := HelloV2(); got != want {
+		t.Errorf("HelloV2() = %q,want %q.", got, want)
 	}
 }
